5884581c4048a8815b26408425b6cca14e000133
resourcefulmind <61961225+resourcefulmind@users.noreply.github.com>
2020-03-29 12:20:04 -1200

    Create index.js
    Task on IF, ELSE and IF ELSE statements for JS

---
+++ index.js
@@ -0,0 +1,10 @@
+var age = 18;
+console.log(age)
+if (age > 18) {
+    console.log("You are older than 18")
+}
+else if (age === 18) {
+    console.log("Happy 18th Birthday")
+} else {
+    console.log("You are still a minor")
+}
