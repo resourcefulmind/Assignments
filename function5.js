e7dcef8a8e90f3495bdead3048056e7b7e528c0e
resourcefulmind <61961225+resourcefulmind@users.noreply.github.com>
2020-05-02 11:22:45 -1200

    put question at the top to enable me keep focus

--- function5.js
+++ function5.js
@@ -1,3 +1,8 @@
+//Fifth assignment:
+//Create a function that takes in array of numbers.
+//The function should multiply all elements of the array by 2
+//and return the modified array
+
 function multiplyAll(myArray) {
   let newArray = [];
   for (var i = 0; i < myArray.length; i++) {
