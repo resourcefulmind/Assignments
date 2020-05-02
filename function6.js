e66a49045041354b702d2e563df9f3442690505b
resourcefulmind <61961225+resourcefulmind@users.noreply.github.com>
2020-05-02 11:25:02 -1200

    added question to the top of 6

--- function6.js
+++ function6.js
@@ -1,3 +1,9 @@
+//Sixth assignment:
+//Create a function that takes in 2 arguments. The first argument is an array
+// and the second argument takes in a value.
+//The function should push whatever value from the 2nd arguments into the array
+//and return the modified array
+
 function addValue(myArray, b) {
   let newArray = [];
   var newArray = myArray.push(b);
