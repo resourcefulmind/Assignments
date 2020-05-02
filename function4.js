a7476a4f5fd9533baa1018351fa26038eaaaf216
resourcefulmind <61961225+resourcefulmind@users.noreply.github.com>
2020-05-02 11:21:38 -1200

    added the question to the top

--- function4.js
+++ function4.js
@@ -1,3 +1,7 @@
+//Fourth assignment:
+//Create a function that takes in 3 arguments.
+// The function should return an addition of all 3 numbers
+
 function addNum(x, y, z) {
   return x + y + z;
 }
